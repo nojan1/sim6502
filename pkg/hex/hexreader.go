ff1e6167b59a0cb8d9b90a71775cf95b19d89a5b
Chris Bearman <chris@chrisjb.com>
2023-09-23 08:09:13 -0500

    checksum

--- pkg/hex/hexreader.go
+++ pkg/hex/hexreader.go
@@ -9,15 +9,24 @@ import (
 	"strings"
 )
 
+// HexChunk represents a chunk of hex data and the address associated with it
 type HexChunk struct {
+	// Addr is the address for the data
 	Addr uint16
+
+	// Data is the raw binary data
 	Data []uint8
 }
 
+// HexReader can read intel hex file format, at least as far as needed for 8 bit
 type HexReader struct {
+	// Chunks are the various chunks of data loaded by the reader
 	Chunks []HexChunk
 }
 
+// NexHexReader will return a hex reader with the content from the specified
+// reader decoded from intel hex format into individual chunks
+// Supports only I8HEX hex files (records 0,1)
 func NewHexReader(r io.Reader) (*HexReader, error) {
 	hr := &HexReader{}
 	ln := 0
@@ -29,28 +38,33 @@ func NewHexReader(r io.Reader) (*HexReader, error) {
 		line := sc.Text()
 		parts := strings.SplitN(line, ":", 2)
 		if len(parts) != 2 {
-			// Line has no colon
+			// Line has no colon, we don't want it
 			continue
 		}
 		ln++
 		hexContent := strings.TrimSpace(parts[1])
 		data, err := hex.DecodeString(hexContent)
 		if err != nil {
-			return nil, fmt.Errorf("HEX invalid HEX content, line %d", ln)
+			return nil, fmt.Errorf("HEX error, invalid HEX content, line %d", ln)
 		}
 
 		if len(data) < 5 {
-			return nil, fmt.Errorf("HEX error, line %d, line too short", ln)
+			return nil, fmt.Errorf("HEX error, line too short, line %d", ln)
 		}
 
 		bc := data[0]
 		addr := (uint16(data[1]) << 8) | uint16(data[2])
 		rt := data[3]
+		cs := calculateChecksum(data)
+		if data[len(data)-1] != cs {
+			return nil, fmt.Errorf("checksum error, line %d", ln)
+		}
 		switch rt {
 		case 0x01:
 			// EOF
 			return hr, nil
 		case 0x00:
+			// 8 bit data
 			if len(data) != int(bc)+5 {
 				return nil, fmt.Errorf("HEX error, line %d, line length incorrect", ln)
 			}
@@ -59,24 +73,29 @@ func NewHexReader(r io.Reader) (*HexReader, error) {
 				Data: data[4 : len(data)-1],
 			}
 			hr.Chunks = append(hr.Chunks, ch)
-			// TODO: Should probably check the checksum
-
-			// fmt.Printf("Loaded -> 0x%04x: %s\n", addr, hex.EncodeToString(ch.Data))
 
 		case 0x02:
-			return nil, errors.New("HEX error, ext seg addr unsupported")
+			return nil, fmt.Errorf("HEX error, record type 2 (extended segment address) unsupported, line %d", ln)
 		case 0x03:
-			return nil, errors.New("HEX error, start seg addr unsupported")
+			return nil, fmt.Errorf("HEX error, record type 3 (start segment address) unsupported, line %d", ln)
 		case 0x04:
-			return nil, errors.New("HEX error, ext linear addr unsupported")
+			return nil, fmt.Errorf("HEX error, record type 4 (extended linear address) unsupported, line %d", ln)
 		case 0x05:
-			return nil, errors.New("HEX error, start linear addr unsupported")
+			return nil, fmt.Errorf("HEX error, record type 5 (start linear address) unsupported, line %d", ln)
 		default:
-			return nil, fmt.Errorf("HEX error, unsupported record type 0x%02x", rt)
+			return nil, fmt.Errorf("HEX error, unsupported record type 0x%02x, line %d", rt, ln)
 		}
-
 	}
 
-	return nil, errors.New("HEX error, no EOF marker")
+	return nil, errors.New("HEX error, no EOF marker found")
 
+}
+
+// calculateChecksum calculates the checksum for a hex record
+func calculateChecksum(data []byte) uint8 {
+	var cs uint8
+	for _, b := range data[:len(data)-1] {
+		cs += uint8(b)
+	}
+	return (cs ^ 0xff) + 1
 }
